64a752ea3440f4a45cb8cfe0c62f0f9a65920e5b
Nuttapon <emperol2@gmil.com>
2013-05-06 12:10:17 +0700

    Revert "Revert "create letustest project""
    This reverts commit 8dffb8ef11950a254dc08777678029ba91934b45.

---
+++ letustest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letustest::Application.config.secret_token = 'cfa00a6aa883cee39d3a997848c955c3d93ab88a289558b774aa5271a5589b19784707a4ac8cf4f26aa15aa2f43311a7a6c1afd82d3e60af3c0ea9eb8910b80e'
