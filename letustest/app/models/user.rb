b44baaf1d97a9becb478b41361bcf2a2a288fe24
Nuttapon <emperol2@gmil.com>
2013-05-06 21:53:43 +0700

    added new authentication
    full functionalities of username and password

--- letustest/app/models/user.rb
+++ letustest/app/models/user.rb
@@ -10,11 +10,17 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  
+  before_save { |user| user.email = email.downcase }
   
   #validates name and email (attributes)
   #the attribute declares as :XXX wherer XXX = name of attribute
   #the validation declares as XXX: where XXX = validation command 
   validates :name, presence: true, length: { maximum: 10 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { maximum: 6 }
+  validates :password_confirmation, presence: true
 end
