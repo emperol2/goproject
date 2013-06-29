2c1ab85ea3f4c16fb15f22b0cd0167834eca96b3
Nuttapon <emperol2@gmil.com>
2013-06-29 18:23:15 +0700

    integrated paypal payment

--- letustest/app/models/feedback.rb
+++ letustest/app/models/feedback.rb
@@ -1,3 +1,21 @@
 class Feedback < ActiveRecord::Base
   attr_accessible :duedate, :email, :firstname, :lastname, :numofbugs, :questions, :telephone, :url
+  
+  def paypal_url(return_url)
+    values = {
+      :business => 'seller@letustest.com',
+      :cmd => '_cart',
+      :upload => 1,
+      :return => return_url,
+      :invoice => id,
+      }
+      values.merge!({
+        # test amount
+      "amount_1" => 20.00, #amount
+      "item_name_1" => 'test', #name of item
+      "item_number_1" => 1, 
+      "quantity_1" => '1'
+      })
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+  end
 end
