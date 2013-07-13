f99023c8412121d69ebf446f71590535e507f9b5
Nuttapon <emperol2@gmil.com>
2013-07-13 17:21:27 +0700

    add secure password for tester model

--- letustest/app/models/tester.rb
+++ letustest/app/models/tester.rb
@@ -1,3 +1,16 @@
 class Tester < ActiveRecord::Base
-  attr_accessible :aboutme, :bankaccount, :city, :country, :email, :fname, :gender, :linkedinid, :lname, :paypalid, :phone, :twitterid
+  attr_accessible :aboutme, :bankaccount, :city, :country, :email, :fname, :gender, :linkedinid, :lname, :paypalid, :phone, :twitterid, :password, :password_confirmation
+
+  has_secure_password
+  
+  #before_save { |user| user.email = email.downcase }
+  
+  #validates name and email (attributes)
+  #the attribute declares as :XXX wherer XXX = name of attribute
+  #the validation declares as XXX: where XXX = validation command 
+  #validates :name, presence: true, length: { maximum: 10 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { maximum: 6 }, :on => :create
+  validates :password_confirmation, presence: true, :on => :create
 end
--- letustest/db/schema.rb
+++ letustest/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713071347) do
+ActiveRecord::Schema.define(:version => 20130713094712) do
 
   create_table "feedbacks", :force => true do |t|
     t.string   "url"
@@ -32,16 +32,6 @@ ActiveRecord::Schema.define(:version => 20130713071347) do
     t.text     "testinginstr"
     t.string   "status"
     t.integer  "user_id"
-  end
-
-  create_table "payment_notifications", :force => true do |t|
-    t.text     "params"
-    t.integer  "feedback_id"
-    t.string   "status"
-    t.string   "transaction_id"
-    t.string   "create"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
   end
 
   create_table "playgrounds", :force => true do |t|
@@ -68,8 +58,9 @@ ActiveRecord::Schema.define(:version => 20130713071347) do
     t.string   "paypalid"
     t.string   "bankaccount"
     t.text     "aboutme"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   create_table "users", :force => true do |t|
